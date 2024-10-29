ef48f0751975fcb5abac2085e2cc1a3c44c86edb
MariaSvard <mariasvard@hotmail.com>
2024-10-29 20:43:08 +0100

    init commit

---
+++ lektion2.py
@@ -0,0 +1,4 @@
+bas = 2
+exponent = 3
+results = bas ** exponent
+print(" Results is : " , results )
---
+++ variabler.py
@@ -0,0 +1,22 @@
+x = 5
+
+print(x)
+x = 5
+y = 3.14 #float
+namn = "Bobbo" #sträng
+is_student = True #bolean
+
+frukter = ["äpple", "banan", "päron", 8] #lista
+
+koordinater = (10, 20) #tuple
+
+person = {"namn": "Bobbo", "ålder": 37}#dictionary
+
+tal = {1, 2, 3, 4, 5, 5}#sets
+print(tal)
+
+a = 10
+b = 5 
+
+produkt = a * b
+print(produkt)
